57672453d28c9b5323ddc0165528f973d6e6c85a
Mihai Nicolae <mihai.nicolae@antebit.com>
2022-05-19 10:49:51 +0300

    Add sample test

--- tabs/src/data/validator.test.js
+++ tabs/src/data/validator.test.js
@@ -1,5 +1,5 @@
-/*const validator = require('./validator');
+const validator = require('./validator');
 
 test('validateName', () => {
-    expect(validator.validateName('te')).toBe('Please enter at least 2 characters!');
-});*/
+    expect(validator.validateName('t')).toBe('Please enter at least 2 characters!');
+});
