95649c58c6797fa7de9b8f79c282d0d90705085b
Mihai Nicolae <mihai.nicolae@antebit.com>
2022-10-10 21:58:55 +0300

    Fix configuration

--- tabs/src/data/sharepointProvider.js
+++ tabs/src/data/sharepointProvider.js
@@ -1,14 +1,11 @@
 import { apiGet, getConfiguration } from './apiProvider';
 
-const sharepointSiteId =
-  '7lcpdm.sharepoint.com,bf9359de-0f13-4b00-8b5a-114f6ef3bfb0,6609a994-5225-4a1d-bd05-a239c7b45f72';
-
 export async function getOrganisationList(country) {
   const config = await getConfiguration();
   try {
     let path =
       '/sites/' +
-      sharepointSiteId +
+      config.SharepointSiteId +
       '/lists/' +
       config.OrganisationListId +
       '/items?$expand=fields';
@@ -36,13 +33,13 @@ export async function getSPUserByMail(email) {
   const config = await getConfiguration();
   try {
     const path =
-        '/sites/' +
-        sharepointSiteId +
-        '/lists/' +
-        config.UserListId +
-        "/items?$filter=fields/Email eq '" +
-        email +
-        "'&$expand=fields",
+      '/sites/' +
+      config.SharepointSiteId +
+      '/lists/' +
+      config.UserListId +
+      "/items?$filter=fields/Email eq '" +
+      email +
+      "'&$expand=fields",
       response = await apiGet(path),
       profile = response.graphClientMessage;
     if (profile.value && profile.value.length) {
